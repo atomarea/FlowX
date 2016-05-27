370ea4541f396c8a42a628fae1f7006ef788da06
DoM!niC <info@dom1nic.eu>
2016-05-27 12:21:54 +0200

    avatar fixes / add 'create conference' dialog

--- src/main/java/net/atomarea/flowx/services/XmppConnectionService.java
+++ src/main/java/net/atomarea/flowx/services/XmppConnectionService.java
@@ -1833,6 +1833,9 @@ public class XmppConnectionService extends Service implements OnPhoneContactsLoa
         account.pendingConferenceLeaves.remove(conversation);
         if (account.getStatus() == Account.State.ONLINE) {
             conversation.resetMucOptions();
+            if (onConferenceJoined != null) {
+                conversation.getMucOptions().flagNoAutoPushConfiguration();
+            }
             conversation.setHasMessagesLeftOnServer(false);
             fetchConferenceConfiguration(conversation, new OnConferenceConfigurationFetched() {
 
@@ -2050,8 +2053,7 @@ public class XmppConnectionService extends Service implements OnPhoneContactsLoa
                     }
                     return;
                 }
-                String localpart = new BigInteger(75, getRNG()).toString(32);
-                Jid jid = Jid.fromParts(localpart, server, null);
+                final Jid jid = Jid.fromParts(new BigInteger(64, getRNG()).toString(Character.MAX_RADIX), server, null);
                 final Conversation conversation = findOrCreateConversation(account, jid, true);
                 joinMuc(conversation, new OnConferenceJoined() {
                     @Override
@@ -2060,7 +2062,7 @@ public class XmppConnectionService extends Service implements OnPhoneContactsLoa
                             @Override
                             public void onPushSucceeded() {
                                 if (subject != null && !subject.trim().isEmpty()) {
-                                    pushSubjectToConference(conversation, subject);
+                                    pushSubjectToConference(conversation, subject.trim());
                                 }
                                 for (Jid invite : jids) {
                                     invite(conversation, invite);
@@ -2068,6 +2070,7 @@ public class XmppConnectionService extends Service implements OnPhoneContactsLoa
                                 if (account.countPresences() > 1) {
                                     directInvite(conversation, account.getJid().toBareJid());
                                 }
+                                saveConversationAsBookmark(conversation, subject);
                                 if (callback != null) {
                                     callback.success(conversation);
                                 }
@@ -2075,6 +2078,7 @@ public class XmppConnectionService extends Service implements OnPhoneContactsLoa
 
                             @Override
                             public void onPushFailed() {
+                                archiveConversation(conversation);
                                 if (callback != null) {
                                     callback.error(R.string.conference_creation_failed, conversation);
                                 }
@@ -3309,6 +3313,21 @@ public class XmppConnectionService extends Service implements OnPhoneContactsLoa
             }
         }
         return templates;
+    }
+
+    public void saveConversationAsBookmark(Conversation conversation, String name) {
+        Account account = conversation.getAccount();
+        Bookmark bookmark = new Bookmark(account, conversation.getJid().toBareJid());
+        if (!conversation.getJid().isBareJid()) {
+            bookmark.setNick(conversation.getJid().getResourcepart());
+        }
+        if (name != null && !name.trim().isEmpty()) {
+            bookmark.setBookmarkName(name.trim());
+        }
+        bookmark.setAutojoin(getPreferences().getBoolean("autojoin", true));
+        account.getBookmarks().add(bookmark);
+        pushBookmarks(account);
+        conversation.setBookmark(bookmark);
     }
 
     public interface OnMamPreferencesFetched {
