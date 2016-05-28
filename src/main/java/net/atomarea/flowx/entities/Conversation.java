55a7f4be18a00729b83d6a222c12ab35a609da35
DoM!niC <info@dom1nic.eu>
2016-05-28 18:19:32 +0200

    Ping Idle Stuff added / GEO URL Support improved / other fixes

--- src/main/java/net/atomarea/flowx/entities/Conversation.java
+++ src/main/java/net/atomarea/flowx/entities/Conversation.java
@@ -677,8 +677,9 @@ public class Conversation extends AbstractEntity implements Blockable {
                     && mode == MODE_SINGLE
                     && axolotlService.isConversationAxolotlCapable(this)
                     && getAccount().getSelfContact().getPresences().allOrNonSupport(AxolotlService.PEP_DEVICE_LIST_NOTIFY)
-                    && getContact().getPresences().allOrNonSupport(AxolotlService.PEP_DEVICE_LIST_NOTIFY)) {
-                return Message.ENCRYPTION_AXOLOTL;
+                    && getContact().getPresences().allOrNonSupport(AxolotlService.PEP_DEVICE_LIST_NOTIFY)
+                    && Config.AlwayUseOMEMO){
+            return Message.ENCRYPTION_AXOLOTL;
             } else {
                 next = this.getMostRecentlyUsedIncomingEncryption();
             }
