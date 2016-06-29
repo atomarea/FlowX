c9e883147e786b76f94db5fcce56f5b6553f60ff
DoM!niC <info@dom1nic.eu>
2016-06-29 15:53:27 +0200

    Fixed ShowFullscreenMessageActivity again

--- src/main/java/net/atomarea/flowx/ui/ShowFullscreenMessageActivity.java
+++ src/main/java/net/atomarea/flowx/ui/ShowFullscreenMessageActivity.java
@@ -7,6 +7,8 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -25,7 +27,9 @@ import net.atomarea.flowx.R;
 import uk.co.senab.photoview.PhotoView;
 import uk.co.senab.photoview.PhotoViewAttacher;
 
-public class ShowFullscreenMessageActivity extends Activity {
+public class ShowFullscreenMessageActivity extends AppCompatActivity {
+
+    private static Toolbar mToolbar;
 
     PhotoView mImage;
     FullscreenVideoLayout mVideo;
@@ -37,12 +41,14 @@ public class ShowFullscreenMessageActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fullscreen_message);
+                mToolbar = (Toolbar) findViewById(R.id.toolbar);
+                setSupportActionBar(mToolbar);
         WindowManager.LayoutParams layout = getWindow().getAttributes();
         layout.screenBrightness = 1;
         getWindow().setAttributes(layout);
-        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        supportRequestWindowFeature(Window.FEATURE_ACTION_BAR);
         getWindow().addFlags(layout.FLAG_KEEP_SCREEN_ON);
-        getActionBar().hide();
         if (Build.VERSION.SDK_INT < 16) {
             getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                     WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -51,7 +57,6 @@ public class ShowFullscreenMessageActivity extends Activity {
             int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
             decorView.setSystemUiVisibility(uiOptions);
         }
-        setContentView(R.layout.activity_fullscreen_message);
         mImage = (PhotoView) findViewById(R.id.message_image_view);
         mVideo = (FullscreenVideoLayout) findViewById(R.id.message_video_view);
         mFullscreenbutton = (ImageView) findViewById(R.id.vcv_img_fullscreen);
